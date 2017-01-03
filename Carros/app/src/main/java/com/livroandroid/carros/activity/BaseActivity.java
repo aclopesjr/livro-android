fc18ecea7f267b3513860e2193324c130740107a
DESKTOP-MK6J2T4\Antonio <aclopesjr@hotmail.com>
2017-01-03 15:19:13 -0200

    9.3 - Toolbar

--- Carros/app/src/main/java/com/livroandroid/carros/activity/BaseActivity.java
+++ Carros/app/src/main/java/com/livroandroid/carros/activity/BaseActivity.java
@@ -1,10 +1,20 @@
 package com.livroandroid.carros.activity;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.livroandroid.carros.R;
 
 /**
  * Created by Antonio on 03/01/2017.
  */
 
 public class BaseActivity extends livroandroid.lib.activity.BaseActivity {
+
+    protected void setUpToolbar() {
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
 }
