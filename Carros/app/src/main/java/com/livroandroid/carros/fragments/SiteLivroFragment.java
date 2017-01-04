d37352cf7973f29d74ddf516e7569126f0563783
DESKTOP-MK6J2T4\Antonio <aclopesjr@hotmail.com>
2017-01-04 16:28:42 -0200

    11.2 - Swipe to Refresh

--- Carros/app/src/main/java/com/livroandroid/carros/fragments/SiteLivroFragment.java
+++ Carros/app/src/main/java/com/livroandroid/carros/fragments/SiteLivroFragment.java
@@ -4,6 +4,7 @@ package com.livroandroid.carros.fragments;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ import com.livroandroid.carros.R;
 public class SiteLivroFragment extends BaseFragment {
 
     private static final String URL_SOBRE = "http://www.livroandroid.com.br/sobre.htm";
+    private SwipeRefreshLayout swipeRefreshLayout;
     private WebView webView;
     private ProgressBar progressBar;
 
@@ -29,11 +31,37 @@ public class SiteLivroFragment extends BaseFragment {
         View view = inflater.inflate(R.layout.fragment_site_livro, container, false);
         this.webView = (WebView)view.findViewById(R.id.webview);
         this.progressBar = (ProgressBar)view.findViewById(R.id.progress);
+        this.swipeRefreshLayout = (SwipeRefreshLayout)view.findViewById(R.id.swipeToRefresh);
 
-        setWebViewClient(this.webView);
+        // Configura o WebViewClient
+        this.webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+                swipeRefreshLayout.setRefreshing(false);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
 
         // Carrega a página
         this.webView.loadUrl(URL_SOBRE);
+
+        // Configura o Swipe Refresh
+        this.swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+            }
+        });
+
+        // Cores da animação do refresh
+        this.swipeRefreshLayout.setColorSchemeResources(R.color.refresh_progress_1, R.color.refresh_progress_2, R.color.refresh_progress_3);
 
         return view;
     }
